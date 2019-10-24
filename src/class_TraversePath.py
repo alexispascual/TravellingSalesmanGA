be8fe9d778f7411f3736100547ed2bdebfd2d6ef
ppascual <ppascual@jpl.nasa.gov>
2019-10-24 16:08:56 -0700

    Adding class file for each path

---
+++ src/class_TraversePath.py
@@ -0,0 +1,43 @@
+import random
+
+class TraversePath(object):
+
+	def __init__(self, num_locations):
+
+		self.fitness_cost = 0
+		self.traverse_path = self.generatePath(num_locations)
+
+	@staticmethod
+	def generateRandomPath(num_locations):
+
+		traverse_path = []
+		point = random.randint(0, num_locations)
+
+		if point not in traverse_path:
+			traverse_path.append(point)
+		else:
+			return traverse_path
+
+	@staticmethod
+	def calculateCost(locations, score):
+
+		for index, waypoint in enumerate(traverse_path):
+
+			if index == (len(traverse_path) - 1):
+				break;
+
+			traverse_distance += calculateDistance(locations[waypoint], locations[index + 1])
+
+		self.fitness_cost = traverse_distance
+			
+
+	@staticmethod
+	def calculateDistance(point_a, point_b):
+
+		euclidian_distance = math.sqrt((point_a[0] - point_b[0])**2 + (point_a[0] - point_b[0])**2)
+
+		return euclidian_distance
+
+	@staticmethod
+	def mutateSelf(parent):
+		
